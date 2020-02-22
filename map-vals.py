b96eaa895771d3de00110d767e75425e776b8d46
Holland.David.1282596420 <david.a.holland.ctr@nga.mil>
2020-02-22 14:25:12 -0500

    The map-vals.py tool now performs a mapping of the english-names onto the auto-generated names in the manifest..

--- map-vals.py
+++ map-vals.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 
+import csv
 import json
 import argparse
 
-iconUniqueIds = {}
+prefix = "icon-" # Should be passed in as arg?
+
+csvRows = []
 transformedJsonObject = {}
+
+def parseCsv(csvFile):
+    with open(csvFile, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            csvRows.append(row)
+
+# key is a base-1 integer but csv rows are base-0.  Subtract 1 to get the correct value.
+def lookupNameValue(key):
+    key -= 1
+    return csvRows[key][0]
 
 def map(inputFile, outputFile):
     with open(inputFile, 'r') as file:
         jsonData = json.loads(file.read())
         for key in jsonData:
-            transformedJsonObject["t-"+key] = jsonData[key]
+            transformedJsonObject[lookupNameValue(int(key[5:]))] = jsonData[key]
     with open(outputFile, 'w') as file:
         json.dump(transformedJsonObject, file, indent=2)
 
@@ -18,5 +32,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('inputFile')
     parser.add_argument('outputFile', nargs = '?')
+    parser.add_argument('csvFile')
+
     args = parser.parse_args()
+    parseCsv(args.csvFile)
     map(args.inputFile,args.outputFile)
